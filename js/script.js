409e3cfdee5ba4d20f16abea0d7adb4d8d6f39ee
Marko Raicevic <markoraicevic649@gmail.com>
2022-10-20 16:55:32 -0500

    changed some details in general

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*typing animation*/
 var typed = new Typed(".typing",{
-    strings:["","Student","Web Developer","Database developer"],
+    strings:["","Student","Web Developer","Database Developer","Data Analyst"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
